5103d104b8fb4b47b22a89b96b82362516e7aa29
juanladeira <juan.fll@hotmail.com>
2025-06-26 20:04:17 -0300

    fix: corrigindo importações

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 
-from .db import create_db_and_tables
-from .routers import task_router
+from app.db import create_db_and_tables
+from app.routers import task_ui_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,4 +15,4 @@ app = FastAPI(lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-app.include_router(task_router.router)
+app.include_router(task_ui_router.router)
